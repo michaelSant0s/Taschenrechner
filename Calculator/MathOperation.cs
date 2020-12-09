9695d42319a4e691350fbfd20bf3dbfa3a5c028f
michaelSant0s <55931967+michaelSant0s@users.noreply.github.com>
2020-12-09 14:10:26 +0100

    Update MathOperation.cs

--- Calculator/MathOperation.cs
+++ Calculator/MathOperation.cs
@@ -124,9 +124,9 @@ namespace Calculator
         ╚═─5
         
         If at the current node is * or / and the next is + or - then they need to be "changed". 
-        For example "3"-4 needs to be changed with 4/5 in the Order, which is not possible because they are on differnt levels.
+        For example the order of "3"-4 needs to be changed with 4/5, which is not possible because they are on differnt levels.
         The solution for this problem is to change the value of 4 with the result of 4/5 and then seal it, so if there is another
-        operation can not get prioritized wrong by mistake.
+        operation, it can not get prioritized wrong by mistake.
         */
         public MathOperation Balance()
         {
